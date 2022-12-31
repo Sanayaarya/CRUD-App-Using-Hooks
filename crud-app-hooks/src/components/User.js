69d76246500cca29332966185a984e352aefd150
Sanaya Arya <sanayaarya0123@gmail.com>
2022-12-31 12:57:48 +0530

    Completed

---
+++ crud-app-hooks/src/components/User.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+function User({ user, removeUser, editUser, index }) {
+  return (
+    <li>
+      <div>
+        <h3>{user.name}</h3>
+        <p>{user.username}</p>
+      </div>
+      <Link to={{
+                  pathname: `/edit/${user.username}`,
+                  id: index
+                }} >
+                  <button>Edit</button>
+      </Link>
+      <span
+        onClick={() => removeUser(index) }
+      >
+        X
+      </span>
+    </li>
+  );
+}
+
+export default User;
---
+++ crud-app-hooks/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import EditUser from './components/EditUser';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Route path="/" exact>
+        <App />
+      </Route>
+      <Route path="/edit/:id" component={EditUser} />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
